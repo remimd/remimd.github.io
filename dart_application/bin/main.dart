1ad5349aeb6a36d3592b9d8e757f27a23f8cd354
remimd <remi.mandart@gmail.com>
2022-01-23 16:38:35 +0100

    feat: Dart Command Line Application

---
+++ dart_application/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application/models.dart' as dart_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application.calculate()}!');
+}
--- dart_application/web/main.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:html';
-
-void main() {
-  querySelector('div#content')?.text = 'Hello world!';
-}
