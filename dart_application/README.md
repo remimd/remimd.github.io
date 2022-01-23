80d93a777eb4b33f45b446c5e59798e4b0e6a726
remimd <remi.mandart@gmail.com>
2022-01-23 17:11:33 +0100

    feat: Dart

--- dart_application/README.md
+++ dart_application/README.md
@@ -2,3 +2,19 @@
 
 A sample command-line application with an entrypoint in `bin/`, library code
 in `lib/`, and example unit test in `test/`.
+
+## Installation
+
+Install dependencies
+
+```bash
+dart pub get
+```
+
+## Usage
+
+Start Application
+
+```bash
+dart bin/main.dart
+```
--- dart_application/bin/main.dart
+++ dart_application/bin/main.dart
@@ -1,5 +1,5 @@
-import 'package:dart_application/models.dart' as dart_application;
+import 'package:dart_application/models.dart' as models;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application.calculate()}!');
+  print('Hello world: ${models.calculate()}!');
 }
